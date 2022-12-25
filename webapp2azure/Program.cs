ba2c4c8bed626aabee93fe86147be4d1c5149f64
maciejw <maciejw@atena.pl>
2022-12-25 19:18:47 +0100

    +Serilog

--- webapp2azure/Program.cs
+++ webapp2azure/Program.cs
@@ -1,4 +1,9 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
